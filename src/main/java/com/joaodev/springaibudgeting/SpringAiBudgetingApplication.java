e102c0dc14aaf55fadd865228e1516fdb3cf4685
Joao Augusto Bonfante <joao.dev93@gmail.com>
2026-09-10 23:08:01 -0300

    initialize Spring AI budgeting application with Gradle setup and basic structure

---
+++ src/main/java/com/joaodev/springaibudgeting/SpringAiBudgetingApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.springaibudgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBudgetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiBudgetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaodev/springaibudgeting/SpringAiBudgetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.springaibudgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiBudgetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
