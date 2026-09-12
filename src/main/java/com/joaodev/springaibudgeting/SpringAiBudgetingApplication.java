5f5ad624dd76bd7849b4448f017373ec317ccc97
Joao Augusto Bonfante <joao.dev93@gmail.com>
2026-09-12 16:41:31 -0300

    add ChatClient integration and create controller for chat prompts

--- src/main/java/com/joaodev/springaibudgeting/SpringAiBudgetingApplication.java
+++ src/main/java/com/joaodev/springaibudgeting/SpringAiBudgetingApplication.java
@@ -1,10 +1,17 @@
 package com.joaodev.springaibudgeting;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiBudgetingApplication {
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiBudgetingApplication.class, args);
---
+++ src/test/java/com/joaodev/springaibudgeting/OpenAiChatClientIT.java
@@ -0,0 +1,26 @@
+package com.joaodev.springaibudgeting;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".*")
+public class OpenAiChatClientIT {
+    @Autowired
+    OpenAiChatModel openAiChatModel;
+
+    @Test
+    void shouldExecuteSumWhenPrompted() {
+        var chatClient = ChatClient.builder(openAiChatModel).defaultSystem("Você é um matemático").build();
+        var response = chatClient.prompt("Qual é a soma de 2 + 2? Exiba apenas o resultado final sem explicações.")
+                .call().content();
+
+        assertThat(response).contains("4");
+    }
+}
